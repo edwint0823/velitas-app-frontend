8f127c0a3e758909aa4c3acfa64a4966f414c47c
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-03-26 10:03:24 -0500

    auth middleware fix

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,11 +5,10 @@ export default async (to, from, next) => {
   const val = token === undefined;
 
   if (val) {
-    // no autenticado1
-    if (["create_order", "search_order"].includes(to.name)) {
+    // no autenticado
+    if (["create_order", "search_order", "login"].includes(to.name)) {
       next();
     } else if (to.name !== "login") next({ name: "login" });
-    next();
   } else {
     // autenticado
     if (to.name === "login") {
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,17 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import auth_routes from "./auth_router.js";
-import order_routes from './orders_router.js'
-import mainRoutes from "./main_routes.js"
+import order_routes from "./orders_router.js";
+import mainRoutes from "./main_routes.js";
 
-const routes = [...auth_routes,...mainRoutes,  ...order_routes]
+const routes = [...order_routes, ...auth_routes, ...mainRoutes];
 const router = createRouter({
-    history: createWebHistory(),
-    // scrollBehavior(to, from, savedPosition) {
-    //     return savedPosition || {left: 0, top: 0};
-    // },
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 router.beforeEach((to) => {
-    const {title} = to.meta;
-    const str = `${title || "INICIO"} | VELITAS APP`;
-    document.title = str.toUpperCase();
-
+  const { title } = to.meta;
+  const str = `${title || "INICIO"} | VELITAS APP`;
+  document.title = str.toUpperCase();
 });
 export default router;
