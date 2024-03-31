e0b74c981c48f70cfd9c26014724162f0209f99d
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-03-31 10:52:48 -0500

    empty component for list

---
+++ src/components/general/EmptyView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img src="/images/empty.svg" alt="empty" class="w-1/2 md:w-1/5" />
+    <p class="text-center text-base font-bold md:text-xl">{{ message }}</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  message: {
+    type: String,
+    required: false,
+    default: "No se encontraron elementos",
+  },
+});
+</script>
+
+<style scoped></style>
